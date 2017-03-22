5012014da197adafced2b1f0c60179508b077fb6
liumeng <liumeng@liumengdeMacBook-Pro.local>
2017-03-22 15:15:58 +0800

    提交code

--- UmengAnalysis.android.js
+++ UmengAnalysis.android.js
@@ -1,31 +1,39 @@
 import { NativeModules } from 'react-native';
-const umengClient = NativeModules.UmengShare;
-var resolveAssetSource = require('../react-native/Libraries/Image/resolveAssetSource');
+const UMNative = NativeModules.UmengNativeModule;
+export default class UmengAnalysis{
 
-export default class UmengShare{
 
-     static setAppKey(value){
-         //umengClient.setAppKey(value);//android无需代码设置
-     }
+  static onEvent(eventId){
+       UMNative.onEvent(eventId);//自定义事件
+  }
+   static onCCEvent(eventId){
+       UMNative.onCCEvent(evenArray, evenValue, eventLabel);// 结构化自定义事件
+  }
+   static onEventWithLabel(eventId){
+      UMNative.onEventWithLabel(eventId, eventLabel);// 自定义事件
+  }
+   static onEventWithParameters(eventId){
+       UMNative.onEventWithParameters(eventId, eventData);//自定义事件
+  }
+   static onEventWithCounter(eventId){
+      UMNative.onEventWithCounter(eventId, eventData, eventNum);//自定义事件
+  }
+   static onPageBegin(eventId){
+       UMNative.onPageBegin(pageName) ;//页面开始的时候调用此方法
+  }
+   static onPageEnd(eventId){
+       UMNative.onPageEnd(pageName) ;//页面结束的时候调用此方法
+  }
+   static profileSignInWithPUID(eventId){
+      UMNative.profileSignInWithPUID(puid);// 统计帐号登录接口
+  }
 
-    static setWXAppId(appId,appSecret){
-        umengClient.setWXAppId(appId,appSecret);
-    }
+  static profileSignInWithPUIDWithProvider(eventId){
+      UMNative.profileSignInWithPUIDWithProvider(puid, provider); //统计帐号登录接口
+  }
+  static profileSignOff(eventId){
+      UMNative.profileSignOff();// 帐号统计退出接口
+  }
 
-    static setQQWithAppId(appId,appSecret){
-      umengClient.setQQZone(appId,appSecret);
-    }
 
-    static setSinaWeibo(appKey,secret){
-        umengClient.setSinaWeibo(appKey,secret);
-    }
-
-    static openShareAction(content,title,url,imageSource){
-        console.log(resolveAssetSource(imageSource))
-        umengClient.openShareAction(content,title,url,resolveAssetSource(imageSource));
-    }
-
-    static showShareMenuView(title, content, url, imageSource){
-        umengClient.openShareAction(content, title, url, resolveAssetSource(imageSource));
-    }
 }
