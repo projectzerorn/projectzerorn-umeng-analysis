8a1887486defb531df6ecb244d707b3dfc8460a3
TFsky <TFsky@huangwenchundeMacBook-Pro.local>
2017-03-23 10:01:30 +0800

    fixed 函数参数

--- UmengAnalysis.ios.js
+++ UmengAnalysis.ios.js
@@ -1,38 +1,40 @@
-import { NativeModules } from 'react-native';
+import {
+  NativeModules
+} from 'react-native';
 const UMNative = NativeModules.UmengNativeModule;
-export default class UmengAnalysis{
+export default class UmengAnalysis {
 
 
-  static onEvent(eventId){
-       UMNative.onEvent(eventId);//自定义事件
+  static onEvent(eventId) {
+    UMNative.onEvent(eventId); //自定义事件
   }
-   static onCCEvent(eventId){
-       UMNative.onCCEvent(evenArray, evenValue, eventLabel);// 结构化自定义事件
+  static onCCEvent(evenArray, evenValue, eventLabel) {
+    UMNative.onCCEvent(evenArray, evenValue, eventLabel); // 结构化自定义事件
   }
-   static onEventWithLabel(eventId){
-      UMNative.onEventWithLabel(eventId, eventLabel);// 自定义事件
+  static onEventWithLabel(eventId, eventLabel) {
+    UMNative.onEventWithLabel(eventId, eventLabel); // 自定义事件
   }
-   static onEventWithParameters(eventId){
-       UMNative.onEventWithParameters(eventId, eventData);//自定义事件
+  static onEventWithParameters(eventId, eventData) {
+    UMNative.onEventWithParameters(eventId, eventData); //自定义事件
   }
-   static onEventWithCounter(eventId){
-      UMNative.onEventWithCounter(eventId, eventData, eventNum);//自定义事件
+  static onEventWithCounter(eventId, eventData, eventNum) {
+    UMNative.onEventWithCounter(eventId, eventData, eventNum); //自定义事件
   }
-   static onPageBegin(eventId){
-       UMNative.onPageBegin(pageName) ;//页面开始的时候调用此方法
+  static onPageBegin(pageName) {
+    UMNative.onPageBegin(pageName); //页面开始的时候调用此方法
   }
-   static onPageEnd(eventId){
-       UMNative.onPageEnd(pageName) ;//页面结束的时候调用此方法
+  static onPageEnd(pageName) {
+    UMNative.onPageEnd(pageName); //页面结束的时候调用此方法
   }
-   static profileSignInWithPUID(eventId){
-      UMNative.profileSignInWithPUID(puid);// 统计帐号登录接口
+  static profileSignInWithPUID(puid) {
+    UMNative.profileSignInWithPUID(puid); // 统计帐号登录接口
   }
 
-  static profileSignInWithPUIDWithProvider(eventId){
-      UMNative.profileSignInWithPUIDWithProvider(puid, provider); //统计帐号登录接口
+  static profileSignInWithPUIDWithProvider(puid, provider) {
+    UMNative.profileSignInWithPUIDWithProvider(puid, provider); //统计帐号登录接口
   }
-  static profileSignOff(eventId){
-      UMNative.profileSignOff();// 帐号统计退出接口
+  static profileSignOff() {
+    UMNative.profileSignOff(); // 帐号统计退出接口
   }
 
 
