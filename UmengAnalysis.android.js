aa4d52f9c10d83f90a7077c492e8f0d00d271a21
liumeng <liumeng@liumengdeMacBook-Pro.local>
2017-03-23 09:58:04 +0800

    push

--- UmengAnalysis.android.js
+++ UmengAnalysis.android.js
@@ -6,32 +6,32 @@ export default class UmengAnalysis{
   static onEvent(eventId){
        UMNative.onEvent(eventId);//自定义事件
   }
-   static onCCEvent(eventId){
+   static onCCEvent(evenArray, evenValue, eventLabel){
        UMNative.onCCEvent(evenArray, evenValue, eventLabel);// 结构化自定义事件
   }
-   static onEventWithLabel(eventId){
+   static onEventWithLabel(eventId, eventLabel){
       UMNative.onEventWithLabel(eventId, eventLabel);// 自定义事件
   }
-   static onEventWithParameters(eventId){
+   static onEventWithParameters(eventId, eventData){
        UMNative.onEventWithParameters(eventId, eventData);//自定义事件
   }
-   static onEventWithCounter(eventId){
+   static onEventWithCounter(eventId, eventData, eventNum){
       UMNative.onEventWithCounter(eventId, eventData, eventNum);//自定义事件
   }
-   static onPageBegin(eventId){
+   static onPageBegin(pageName){
        UMNative.onPageBegin(pageName) ;//页面开始的时候调用此方法
   }
-   static onPageEnd(eventId){
+   static onPageEnd(pageName){
        UMNative.onPageEnd(pageName) ;//页面结束的时候调用此方法
   }
-   static profileSignInWithPUID(eventId){
+   static profileSignInWithPUID(puid){
       UMNative.profileSignInWithPUID(puid);// 统计帐号登录接口
   }
 
-  static profileSignInWithPUIDWithProvider(eventId){
+  static profileSignInWithPUIDWithProvider(puid, provider){
       UMNative.profileSignInWithPUIDWithProvider(puid, provider); //统计帐号登录接口
   }
-  static profileSignOff(eventId){
+  static profileSignOff(){
       UMNative.profileSignOff();// 帐号统计退出接口
   }
 
