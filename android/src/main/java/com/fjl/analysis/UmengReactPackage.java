a96696945bddf34e3ea35c74b71f9b26cc930336
projectzerorn <jackzhou.projectzero@gmail.com>
2017-11-28 17:08:27 +0800

    Update UmengReactPackage.java
    去掉无效代码

--- android/src/main/java/com/fjl/analysis/UmengReactPackage.java
+++ android/src/main/java/com/fjl/analysis/UmengReactPackage.java
@@ -26,10 +26,6 @@ public class UmengReactPackage implements ReactPackage {
         return modules;
     }
 
-    @Override
-    public List<Class<? extends JavaScriptModule>> createJSModules() {
-        return Collections.emptyList();
-    }
 
     @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
